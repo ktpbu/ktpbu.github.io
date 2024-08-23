5204c8679142951acd3eb31f4a8cc7ace59d483a
Thomas Boles <thomas211738@gmail.com>
2024-08-22 21:00:41 -0400

    added privacy policy for ktp app

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import { Contact, Home, Rush, Members, HackKTP, AboutKTP, NotFound } from './pages';
+import { Contact, Home, Rush, Members, HackKTP, AboutKTP, NotFound, Privacy_Policy } from './pages';
 
 export const Navigation = () => {
     return (
@@ -11,6 +11,7 @@ export const Navigation = () => {
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/hackktp" element={<HackKTP />} />
                 <Route path="/aboutktp" element={<AboutKTP />} />
+                <Route path="/privacy_policy" element={<Privacy_Policy />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
